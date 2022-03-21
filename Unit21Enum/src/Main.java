d8215ff62274e6a0a1044660d036d04048bc3be5
AnatolSalau <salovanatoly5@mail.ru>
2022-03-21 14:50:20 +0300

    Add lesson 21

--- Unit21Enum/src/Main.java
+++ Unit21Enum/src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Day current = Day.MONDAY;
+        System.out.println(current);    // MONDAY
+
+        Book b1 = new Book("War and Peace", "L. Tolstoy", Type.BELLETRE);
+        System.out.printf("Book '%s' has a type %s", b1.name, b1.bookType);
+
+        switch(b1.bookType){
+            case BELLETRE:
+                System.out.println("Belletre");
+                break;
+            case SCIENCE:
+                System.out.println("Science");
+                break;
+            case SCIENCE_FICTION:
+                System.out.println("Science fiction");
+                break;
+            case PHANTASY:
+                System.out.println("Phantasy");
+                break;
+        }
+        System.out.println(Type.BELLETRE.ordinal());
+
     }
 }
